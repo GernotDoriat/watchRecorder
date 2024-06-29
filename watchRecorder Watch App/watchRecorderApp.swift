b84c2d96dc33b69b9ac88edf34cbfd362245c1a8
Gernot Doriat <gernot.doriat@web.de>
2024-06-29 19:53:52 +0200

    Initial Commit

---
+++ watchRecorder Watch App/watchRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchRecorderApp.swift
+//  watchRecorder Watch App
+//
+//  Created by Gernot Doriat on 29.06.24.
+//
+
+import SwiftUI
+
+@main
+struct watchRecorder_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchRecorder/watchRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchRecorderApp.swift
+//  watchRecorder
+//
+//  Created by Gernot Doriat on 29.06.24.
+//
+
+import SwiftUI
+
+@main
+struct watchRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
