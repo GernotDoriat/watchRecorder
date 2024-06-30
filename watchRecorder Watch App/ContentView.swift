69d69bdf7110a8f9295b87e621ad1e69d42892ee
Gernot Doriat <gernot.doriat@web.de>
2024-06-30 17:35:10 +0200

    auto stop

--- watchRecorder Watch App/ContentView.swift
+++ watchRecorder Watch App/ContentView.swift
@@ -9,25 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isRecording = false
+    
     var body: some View {
         VStack {
             Button(action: {
                 toggleRecording()
             }) {
                 Text(isRecording ? "Stop Recording" : "Start Recording")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity) // Füllt den verfügbaren Platz aus
+                    .padding() // Fügt Padding um den Text hinzu
+                    .background(isRecording ? Color.red : Color.green) // Ändert die Hintergrundfarbe je nach Zustand
+                    .foregroundColor(.black) // Setzt die Textfarbe auf weiß
+                    .cornerRadius(30) // Optional: Fügt abgerundete Ecken hinzu
             }
-            .background(Color.green) // Setzt die Hintergrundfarbe auf grün
-            .foregroundColor(.black)
-            .cornerRadius(30) // Optional: Fügt abgerundete Ecken hinzu
-            
-            .padding()
+            .padding() // Fügt Padding um den Button hinzu
         }
-        .padding()
+        .padding() // Fügt Padding um den VStack hinzu
     }
     
     func toggleRecording() {
         debugPrint("toggleRecording")
-      
+        isRecording.toggle() // Schaltet den Aufzeichnungszustand um
+        if isRecording {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+                        isRecording = false
+                    }
+                }s
     }
 }
 
